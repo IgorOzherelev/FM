5782fd14163e16a7059d7136c5d99aa0132feb51
IgorOzherelev <i9163073959@gmail.com>
2021-04-04 13:13:32 +0300

    получение feasible move

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <vector>
 
 #include "utils.h"
 #include "HyperGraph.h"
@@ -38,15 +39,17 @@ public:
     std::uint32_t *right_net_sizes;
 
     std::size_t solution_cost = 0;
-    std::size_t balance = 0;
+    std::int64_t balance = 0;
+    std::size_t imbalance = 0;
 
     ~Partition() = default;
-    Partition(HyperGraph *graph, std::size_t balance) {
+    Partition(HyperGraph *graph, std::size_t imbalance) {
         this->graph = graph;
         this->left_net_sizes = (std::uint32_t*)std::calloc(graph->nets_num, sizeof(std::uint32_t));
         this->right_net_sizes = (std::uint32_t*)std::calloc(graph->nets_num, sizeof(std::uint32_t));
         this->vertices_part = (char*)std::calloc(graph->vex_num, sizeof(char));
         this->balance = graph->vex_num % 2;
+        this->imbalance = imbalance;
 
         init_vertices();
         init_side_sizes();
@@ -76,7 +79,7 @@ public:
         }
     }
 
-    HyperGraph *get_graph(){
+    HyperGraph *get_graph() {
         return this->graph;
     }
 };
@@ -102,7 +105,7 @@ private:
         int vx_gain = 0;
         char cur_vex_side = partition->vertices_part[vex_id];
 
-        std::vector<uint32_t> tmp = partition->get_graph()->vex_to_nets[vex_id];
+        std::vector<std::uint32_t> tmp = partition->get_graph()->vex_to_nets[vex_id];
         for(std::size_t j = 0; j < tmp.size(); j++) {
             if (cur_vex_side == 0) {
                 if (partition->left_net_sizes[tmp[j]] == 1)
@@ -123,11 +126,52 @@ public:
     std::map<int, std::vector<std::uint32_t>> left;
     std::map<int, std::vector<std::uint32_t>> right;
 
+    char *free_array;
+    std::size_t lock_cnt = 0;
 
     ~GainContainer() = default;
     GainContainer(Partition *partition) {
         this->partition = partition;
+        // 0 - вершина свободна, 1 - занята
+        this->free_array = (char*)std::calloc(partition->get_graph()->vex_num, sizeof(char));
         init();
+    }
+
+    bool is_all_locked() {
+        return lock_cnt == partition->get_graph()->vex_num;
+    }
+
+    std::pair<std::uint32_t, int> feasible_move() {
+        auto max_left_bucket = --left.end();
+        auto max_right_bucket = --right.end();
+
+        int max_gain_right = max_right_bucket->first;
+        int max_gain_left = max_left_bucket->first;
+        std::uint32_t moved_vex_id;
+        int moved_vex_gain;
+        // move from right to left
+        if ((max_gain_left < max_gain_right && -partition->balance < partition->imbalance)
+                || (partition->balance >= partition->imbalance)) {
+            moved_vex_id = *(--(max_right_bucket->second).end());
+            max_right_bucket->second.erase(--max_right_bucket->second.end());
+
+            if (max_right_bucket->second.empty())
+                right.erase(max_gain_right);
+
+            moved_vex_gain = max_gain_right;
+        } else { // move from left to right
+            moved_vex_id = *(--(max_left_bucket->second).end());
+            max_left_bucket->second.erase(--max_left_bucket->second.end());
+
+            if (max_left_bucket->second.empty())
+                left.erase(max_gain_left);
+
+            moved_vex_gain = max_gain_left;
+        }
+
+        free_array[moved_vex_id] = 1;
+        lock_cnt++;
+        return {moved_vex_id, moved_vex_gain};
     }
 
 };
@@ -139,6 +183,8 @@ int main(int args, char **argv) {
     HyperGraph graph(filename);
     Partition partition(&graph, 0);
     GainContainer container(&partition);
+
+    auto var = container.feasible_move();
 
     auto end = std::chrono::high_resolution_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " mc" << std::endl;
